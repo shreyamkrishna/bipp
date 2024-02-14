2912321ec5c728cf1c378185b3d84e8b55175606
Shreyam Krishna <kshreyam@daint106.login.cscs.ch>
2024-02-14 10:05:25 +0100

    Added history to the fits file output, and writing image levels to fits files individually now.

--- examples/real_data/test3.py
+++ examples/real_data/test3.py
@@ -27,6 +27,7 @@ import matplotlib.cm as cm
 from matplotlib.colors import TwoSlopeNorm
 from matplotlib.colors import LogNorm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import sys
 plt.rcParams.update({'font.size': 40})
 #"Eg:\npython realData.py [telescope name(string)] [path_to_ms(string)] [output_name(string)] [N_pix(int)] [FoV(float(deg))] [N_levels(int)] [Clustering(bool/list_of_eigenvalue_bin_edges)] [partitions] [WSCleangrid(bool)] [WSCleanPath(string)]"))
 
@@ -518,14 +519,6 @@ if (outputCustomFitsFile):
     w.wcs.crval = np.array([field_center.ra.deg, field_center.dec.deg])
     w.wcs.ctype = ["RA---SIN", "DEC--SIN"]
 
-    #cart = coord.CartesianRepresentation(xyz_grid[:, 0], xyz_grid[:, 1], xyz_grid[:, 2])
-    #sph = coord.SphericalRepresentation.from_cartesian(cart)
-
-    #colat = u.Quantity(90 * u.deg - sph.lat).to_value(u.rad)
-    #lon = u.Quantity(sph.lon).to_value(u.rad)
-
-    #print (colat.max(), lon.max())
-
     header = w.to_header()
     hdu =fits.PrimaryHDU(np.fliplr(I_lsq_eq_summed.data),header=header)
 
@@ -543,28 +536,31 @@ if (outputCustomFitsFile):
     hdu.header['BUNIT'] = 'Jy/Beam' # Units of the data array
     hdu.header['BTYPE'] = 'Intensity'
     hdu.header['ORIGIN'] = "BIPP"
+    hdu.header['HISTORY'] = sys.argv[:]
 
-    hdu.writeto(f"{args.output}.fits", overwrite=True)
+    hdu.writeto(f"{args.output}_summed.fits", overwrite=True)
 
-    hdu =fits.PrimaryHDU(np.fliplr(I_lsq_eq.data),header=header)
+    for i in np.arange(args.nlevel):
+        hdu =fits.PrimaryHDU(np.fliplr(I_lsq_eq.data[i, :, :]),header=header)
 
-    hdu.header['SIMPLE'] = 'T' # fits compliant format
-    if (precision.lower()=='double'):
-        hdu.header['BITPIX']=-64 # double precision float
-    elif (precision.lower()=='single'):
-        hdu.header['BITPIX']=-32 # single precision float
-    hdu.header['NAXIS'] = 2 # Number of axes - 2 for image data, 3 for data cube
-    hdu.header['NAXIS1'] = I_lsq_eq_summed.shape[-2]
-    hdu.header['NAXIS2'] = I_lsq_eq_summed.shape[-1]
-    #shdu.header['EXTEND'] = "T" # Fits data set may contain extensions
-    hdu.header['BSCALE'] = 1 # scale to be multiplied by the data array values when reading the FITS file
-    hdu.header['BZERO'] = 0 # zero offset to be added to the data array values when reading the FITS file
-    hdu.header['BUNIT'] = 'Jy/Beam' # Units of the data array
-    hdu.header['BTYPE'] = 'Intensity'
-    hdu.header['ORIGIN'] = "BIPP"
+        hdu.header['SIMPLE'] = 'T' # fits compliant format
+        if (precision.lower()=='double'):
+            hdu.header['BITPIX']=-64 # double precision float
+        elif (precision.lower()=='single'):
+            hdu.header['BITPIX']=-32 # single precision float
+        hdu.header['NAXIS'] = 2 # Number of axes - 2 for image data, 3 for data cube
+        hdu.header['NAXIS1'] = I_lsq_eq_summed.shape[-2]
+        hdu.header['NAXIS2'] = I_lsq_eq_summed.shape[-1]
+        #shdu.header['EXTEND'] = "T" # Fits data set may contain extensions
+        hdu.header['BSCALE'] = 1 # scale to be multiplied by the data array values when reading the FITS file
+        hdu.header['BZERO'] = 0 # zero offset to be added to the data array values when reading the FITS file
+        hdu.header['BUNIT'] = 'Jy/Beam' # Units of the data array
+        hdu.header['BTYPE'] = 'Intensity'
+        hdu.header['ORIGIN'] = "BIPP"
+        hdu.header['HISTORY'] = sys.argv[:]
 
-    hdu.writeto(f"{args.output}_lvls.fits", overwrite=True)
-    # instead of this, make wcs and store as header
+        hdu.writeto(f"{args.output}_lvl{i}.fits", overwrite=True)
+
 else:
-    I_lsq_eq_summed.to_fits(f"{args.output}.fits")
+    I_lsq_eq_summed.to_fits(f"{args.output}_summed.fits")
     I_lsq_eq.to_fits(f"{args.output}_lvls.fits")
